0ac8f4b000746866581ff458dc4203158230244a
isaacsergenaka <isaacserge.naka@gmail.com>
2026-04-30 15:43:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
+import { createClient } from '@supabase/supabase-js';
 
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
+);
 const SERVICES = [
   {
     id: "mecanique",
